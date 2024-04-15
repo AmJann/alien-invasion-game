3a5949664468c77cfe8bb9f6a260bddc23e6b3c5
Seyamack Afzali <seyamack@gmail.com>
2024-04-15 16:03:34 -0500

    added player attack animation + keybind

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -1,5 +1,5 @@
 import { EventBus } from "../EventBus";
-import { Scene } from "phaser";
+import { Input, Scene } from "phaser";
 import AnimatedTiles from "phaser-animated-tiles-phaser3.5/dist/AnimatedTiles.min.js";
 //sets players current direction
 let currentDirection = "right";
@@ -10,6 +10,7 @@ export class Game extends Scene {
     constructor() {
         super("Game");
         this.player;
+        this.human
     }
     preload() {
         this.load.image(
@@ -57,21 +58,38 @@ export class Game extends Scene {
             "goblin_idle_1.png"
         ));
         //sets size of collision box for player
-        this.player.body.setSize(8, 6);
-        const player2 = this.add.sprite(
-            500,
-            650,
+        this.player.body.setSize(8, 10);
+        const player2 = (this.player2 = this.physics.add.sprite(
+            350,
+            400,
             "player",
             "goblin_hurt_1.png"
-        );
+        ));
+        this.player2.body.setSize(12,16)
+        const human = (this.human = this.physics.add.sprite(
+            400,
+            400,
+            'human',
+            'base_idle_1.png'
+        ));
+        this.human.body.setSize(22,26)
         //creates keys for movement to be used in update funcion further down
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        //sets collisions for player amongst these layers
+        //sets collisions for player and human amongst these layers
         this.physics.add.collider(this.player, layer2);
         this.physics.add.collider(this.player, layer4);
         this.physics.add.collider(this.player, groundLayer1);
-
+        this.physics.add.collider(this.player2, layer2);
+        this.physics.add.collider(this.player2, layer4);
+        this.physics.add.collider(this.player2, groundLayer1);
+        this.physics.add.collider(this.human, layer2);
+        this.physics.add.collider(this.human, layer4);
+        this.physics.add.collider(this.human, groundLayer1);
+        
+        // set collisions for player and NPC
+        this.physics.add.collider(this.player, this.human, )
+        this.physics.add.collider(this.player, this.player2)
         //sets collisions by tile id in layers
         layer2.setCollisionBetween(1, 3000);
         layer4.setCollisionBetween(1, 357);
@@ -130,7 +148,6 @@ export class Game extends Scene {
             repeat: -1,
             frameRate: 12,
         });
-
         this.anims.create({
             key: "player-hurt-right",
             frames: this.anims.generateFrameNames("player", {
@@ -139,23 +156,97 @@ export class Game extends Scene {
                 prefix: "goblin_hurt_",
                 suffix: ".png",
             }),
+            frameRate: 12,
+        });
+        this.anims.create({
+            key: "player-hurt-left",
+            frames: this.anims.generateFrameNames("player", {
+                start: 1,
+                end: 8,
+                prefix: "goblin_hurt_left_",
+                suffix: ".png",
+            }),
+            frameRate: 12,
+        });
+        this.anims.create({
+            key: "player-attack-right",
+            frames: this.anims.generateFrameNames("player", {
+                start: 1,
+                end: 9,
+                prefix: "goblin_attack_",
+                suffix: ".png",
+            }),
             repeat: -1,
             frameRate: 12,
         });
+        this.anims.create({
+            key: "player-attack-left",
+            frames: this.anims.generateFrameNames("player", {
+                start: 1,
+                end: 8,
+                prefix: "goblin_attack_left_",
+                suffix: ".png",
+            }),
+            repeat: -1,
+            frameRate: 12,
+        });
+
+
+        // Human Animations
+        this.anims.create({
+            key: "human-idle-right",
+            frames: this.anims.generateFrameNames("human", {
+                start: 1,
+                end: 9,
+                prefix: "base_idle_",
+                suffix: ".png",
+            }),
+            repeat: -1,
+            frameRate: 12,
+        });
+
+        this.anims.create({
+            key: "human-idle-left",
+            frames: this.anims.generateFrameNames("human", {
+                start: 1,
+                end: 9,
+                prefix: "base_idle_left_",
+                suffix: ".png",
+            }),
+            repeat: -1,
+            frameRate: 12,
+        });
+
+
         player.anims.play("player-walk-right");
         player2.anims.play("player-hurt-right");
-
+        human.anims.play('human-idle-right')
         EventBus.emit("current-scene-ready", this);
     }
 
     changeScene() {
         this.scene.start("GameOver");
     }
-
+    something(direction) {
+        if (direction === 'left'){
+            this.player.anims.play("player-attack-left", true);
+        }
+        else {
+            this.player.anims.play("player-attack-right", true);
+        }
+    }
     update() {
+        // keeps players and NPCs from moving when they collide
+        this.human.setVelocityX(0)
+        this.human.setVelocityY(0)
+        this.player2.setVelocityX(0)
+        this.player2.setVelocityY(0)
+
         //keeps player from continuing to move after pressing key
         this.player.setVelocityX(0);
         this.player.setVelocityY(0);
+       
+
 
         //player walk right
         if (this.cursors.right.isDown) {
@@ -196,6 +287,19 @@ export class Game extends Scene {
         } else if (currentDirection === "right") {
             this.player.setVelocityX(0);
             this.player.anims.play("player-idle-right", true);
+        }
+        // player attack right
+        if (Input.Keyboard.JustDown(this.cursors.space) && currentDirection == 'right') {
+            // we'll need some method on the player sprite
+            this.something('right')
+            
+        }
+        // player attack left
+        if (this.cursors.space.isDown && currentDirection == 'left') {
+            // we'll need some method on the player sprite
+            this.something('left')
+            
+            
         }
     }
 }
