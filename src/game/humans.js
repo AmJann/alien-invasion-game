c4d03f19bc87a32ef3a78f60e6d12d3d5af90f95
Amber Jankowski <amberbjankowski@gmail.com>
2024-04-25 17:37:48 -0400

    humans array of objects created and imported to fight scene loading enemies randomly and dynamically

---
+++ src/game/humans.js
@@ -0,0 +1,94 @@
+const humans = [
+    {
+        name: "Clown",
+        health: 100,
+        attack1: {
+            name: "Slash",
+            damage: 20,
+        },
+        attack2: {
+            name: "Stab",
+            damage: 15,
+        },
+        special: {
+            name: "Glitter Strike",
+            damage: 30,
+        },
+        mainImage:
+            "assets/characters/humans/no-bg-imgs/clown-char/clown-char-14.png",
+    },
+    {
+        name: "Scientist",
+        health: 90,
+        attack1: {
+            name: "Lab Rat Bite",
+            damage: 20,
+        },
+        attack2: {
+            name: "Punch",
+            damage: 15,
+        },
+        special: {
+            name: "Flask Explosion",
+            damage: 35,
+        },
+        mainImage:
+            "assets/characters/humans/no-bg-imgs/scientist-char/4-scientist-char.png",
+    },
+    {
+        name: "Firefighter",
+        health: 110,
+        attack1: {
+            name: "Scorch",
+            damage: 20,
+        },
+        attack2: {
+            name: "Extinguish Punch",
+            damage: 15,
+        },
+        special: {
+            name: "Water Cannon",
+            damage: 30,
+        },
+        mainImage:
+            "assets/characters/humans/no-bg-imgs/firefighter-char/8-firefighter-char.png",
+    },
+    {
+        name: "Farmer",
+        health: 80,
+        attack1: {
+            name: "Pitchfork Punch",
+            damage: 20,
+        },
+        attack2: {
+            name: "Hoe Smack",
+            damage: 15,
+        },
+        special: {
+            name: "Farm Animal Stampede",
+            damage: 35,
+        },
+        mainImage:
+            "assets/characters/humans/no-bg-imgs/farmer-char/8-farmer-char.png",
+    },
+    {
+        name: "Nuck Chorris",
+        health: 130,
+        attack1: {
+            name: "Roundhouse Kick",
+            damage: 25,
+        },
+        attack2: {
+            name: "Uppercut",
+            damage: 20,
+        },
+        special: {
+            name: "1000 fists of fury",
+            damage: 40,
+        },
+        mainImage:
+            "assets/characters/humans/no-bg-imgs/chuck-char/8-chuck-char.png",
+    },
+];
+
+export default humans;
