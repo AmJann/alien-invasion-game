0ff43c7706da31b47030740ac7b338299959b37b
Seyamack Afzali <seyamack@gmail.com>
2024-05-01 12:57:59 -0500

    weapon direction/timing works, collision broken w/ NPC, removed debug statments + formatted

--- src/game/sprites/Player.js
+++ src/game/sprites/Player.js
@@ -6,12 +6,11 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.inventory = [humans[1]];
-        this.currentDirection = 'right'
-        this.currentState = 'walking'
-        this.cursors = this.scene.input.keyboard.createCursorKeys()
-        this.speed = 150
-        //
-        this.weapon = this.scene.physics.add.sprite(-50, -50)
+        this.currentDirection = "right";
+        this.currentState = "walking";
+        this.cursors = this.scene.input.keyboard.createCursorKeys();
+        this.speed = 150;
+        this.weapon = this.scene.physics.add.sprite(-50, -50);
         this.weapon.setSize(30, 15);
         this.weapon.setActive(false).setVisible(false);
     }
@@ -29,10 +28,10 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         }
     }
     swingWeapon(direction) {
-        if (direction == 'right') {
-            this.anims.play('player-attack-right', true)
+        if (direction == "right") {
+            this.anims.play("player-attack-right", true);
         } else {
-            this.anims.play('player-attack-left', true)
+            this.anims.play("player-attack-left", true);
         }
     }
     hasHuman(human) {
@@ -47,7 +46,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         obj.setVisible(true);
 
         obj.setPosition(x, y);
-        
+
         //setTimeout(this.sheathWeapon, 250, obj)
         this.scene.time.delayedCall(300, this.sheathWeapon, [obj]);
     }
@@ -55,30 +54,22 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         obj.setPosition(-50, -50);
     }
     animsManager(direction, state, xVel, yVel) {
-        if (state === 'attacking') {
-            if (direction == 'left') {
-                console.log('attack left')
+        if (state === "attacking") {
+            if (direction == "left") {
                 this.anims.play("player-attack-left", true);
             } else {
-                console.log('attack right')
                 this.anims.play("player-attack-right", true);
             }
-        }
-        else if (state === 'walking' && xVel != 0 || yVel != 0) {
-            if (direction == 'left') {
-                console.log('walk left')
+        } else if ((state === "walking" && xVel != 0) || yVel != 0) {
+            if (direction == "left") {
                 this.anims.play("player-walk-left", true);
             } else {
-                console.log('walk right')
                 this.anims.play("player-walk-right", true);
             }
-            
-        }  else  {
-            if (direction == 'left') {
-                console.log('idle left')
+        } else {
+            if (direction == "left") {
                 this.anims.play("player-idle-left", true);
             } else {
-                console.log('idle right')
                 this.anims.play("player-idle-right", true);
             }
         }
@@ -103,40 +94,45 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         // player walk up with character facing left or right based on current direction
         if (this.cursors.up.isDown && this.currentDirection === "left") {
             velY = -this.speed;
-        } else if (this.cursors.up.isDown && this.currentDirection === "right") {
+        } else if (
+            this.cursors.up.isDown &&
+            this.currentDirection === "right"
+        ) {
             velY = -this.speed;
         }
 
         //player walk down with character facing left or right based on current direction
         if (this.cursors.down.isDown && this.currentDirection === "left") {
             velY = this.speed;
-        } else if (this.cursors.down.isDown && this.currentDirection === "right") {
+        } else if (
+            this.cursors.down.isDown &&
+            this.currentDirection === "right"
+        ) {
             velY = this.speed;
         }
 
         // player attack right
         if (this.cursors.space.isDown) {
-            
-            const xPos = this.body.x;
-            const yPos = this.body.y;
-            console.log('SOMETHING WORKDS')
-            this.currentState = 'attacking'
-            this.swingWeapon(this.currentDirection)
-            if (this.currentDirection === "left") {
-                this.drawWeapon(xPos - 12, yPos - 2, this.weapon);
-            } else {
-                this.drawWeapon(xPos + 12, yPos - 2, this.weapon);
-            }
-            this.scene.time.delayedCall(750, ()=> {this.currentState = 'walking'})
+            this.currentState = "attacking";
+            this.swingWeapon(this.currentDirection);
+            this.scene.time.delayedCall(350, () => {
+                const xPos = this.body.x;
+                const yPos = this.body.y;
+                if (this.currentDirection === "left") {
+                    this.drawWeapon(xPos - 12, yPos - 2, this.weapon);
+                } else {
+                    this.drawWeapon(xPos + 12, yPos - 2, this.weapon);
+                }
+            });
+            this.scene.time.delayedCall(750, () => {
+                this.currentState = "walking";
+            });
         }
-        
-        this.setVelocity(velX, velY);
-        this.animsManager(this.currentDirection, this.currentState, velX, velY)
-        console.log(this.cursors.isDown)
-       
 
+        this.setVelocity(velX, velY);
+        this.animsManager(this.currentDirection, this.currentState, velX, velY);
     }
     update() {
-        super.update()
+        super.update();
     }
 }
