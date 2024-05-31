893fbf9b8e364b078fe071cfb02b925d96defc00
Seyamack Afzali <seyamack@gmail.com>
2024-05-31 09:38:38 -0500

    currently testing with playwright

--- src/tests/player.spec.js
+++ src/tests/player.spec.js
@@ -1,13 +1,24 @@
-import { expect, test, vi } from 'vitest'
-import { Player } from '../game/sprites/Player'
+// import { expect, test, vi } from 'vitest'
+// //import { Player } from '../game/sprites/Player'
 
+// // const GameSandbox = 
 
-test('swingWeapon', () => {
-    let player = Player()
-    const anims = vi.fn((attackName, attackBool) => 0)
-    const clock = vi.fn((timer, funct) => 0)
-    const direction = 'left' 
-    player.swingWeapon(direction, anims, clock)
-    expect(anims.calls).toEqual([['player-attack-left', true]])
-    expect(clock.calls).toEqual([[200, ()=> 'dont break']])
-})
+// // const testPlayer = 
+// class Player{
+//     constructor() {
+        
+//     }
+//     swingWeapon() {
+        
+//     }
+// }
+
+// test('swingWeapon', () => {
+//     let player = Player()
+//     const anims = vi.fn((attackName, attackBool) => 0)
+//     const clock = vi.fn((timer, funct) => 0)
+//     const direction = 'left' 
+//     player.swingWeapon(direction, anims, clock)
+//     expect(anims.calls).toEqual([['player-attack-left', true]])
+//     expect(clock.calls).toEqual([[200, ()=> 'dont break']])
+// })
