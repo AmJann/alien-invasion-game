54d9c322f7418236ee499d9636797e0c7b6bc66c
Seyamack Afzali <seyamack@gmail.com>
2024-06-01 12:46:58 -0500

    almost working unit tests using jest, just a lot of mocking needed maybe

--- src/tests/player.spec.js
+++ src/tests/player.spec.js
@@ -1,24 +1,19 @@
-// import { expect, test, vi } from 'vitest'
-// //import { Player } from '../game/sprites/Player'
+import { expect, test, jest } from '@jest/globals'
+import { Player } from '../game/sprites/Player'
 
-// // const GameSandbox = 
+// const GameSandbox = 
 
-// // const testPlayer = 
-// class Player{
-//     constructor() {
-        
-//     }
-//     swingWeapon() {
-        
-//     }
-// }
+// const testPlayer = 
 
-// test('swingWeapon', () => {
-//     let player = Player()
-//     const anims = vi.fn((attackName, attackBool) => 0)
-//     const clock = vi.fn((timer, funct) => 0)
-//     const direction = 'left' 
-//     player.swingWeapon(direction, anims, clock)
-//     expect(anims.calls).toEqual([['player-attack-left', true]])
-//     expect(clock.calls).toEqual([[200, ()=> 'dont break']])
-// })
+
+test('swingWeapon', () => {
+    const newScene = jest.fn((sceneName) => { })
+    const newTexture = jest.fn((textureString)=>{})
+    let player = new Player(newScene, 0, 0, newTexture)
+    const anims = jest.fn((attackName, attackBool) => 0)
+    const clock = jest.fn((timer, funct) => 0)
+    const direction = 'left' 
+    player.swingWeapon(direction, anims, clock)
+    expect(anims.calls).toEqual([['player-attack-left', true]])
+    expect(clock.calls).toEqual([[200, ()=> 'dont break']])
+})
