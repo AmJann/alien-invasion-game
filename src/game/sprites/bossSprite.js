af800a37ee82dcd23a87bd14a146a917fa0750d5
Seyamack Afzali <seyamack@gmail.com>
2024-06-13 10:15:06 -0500

    updated/working boss sprite

--- src/game/sprites/bossSprite.js
+++ src/game/sprites/bossSprite.js
@@ -6,14 +6,13 @@ export class bossSprite extends Physics.Arcade.Sprite {
 
         scene.sys.updateList.add(this);
         scene.sys.displayList.add(this);
-        this.direction = 1;
-        this.id = id
+        
         this.facing = "right";
         this.currentState = 'walking';
         this.playSoundEffect = true
-        
+        this.flag = true
 
-        //this.setScale(2);
+        this.setScale(1.25);
         scene.physics.add.existing(this);
         scene.physics.world.enableBody(this);
         //this.setImmovable(true);
@@ -140,7 +139,7 @@ export class bossSprite extends Physics.Arcade.Sprite {
     killNPC() {
         if (this.playSoundEffect) {
             this.currentState = 'smacked'
-            console.log(this.currentState)
+            
             this.setVelocity(0, 0)
         
             if (this.facing == "right") {
@@ -155,48 +154,17 @@ export class bossSprite extends Physics.Arcade.Sprite {
         }
     }
     changeDirection() {
-        if (this.currentState != 'walking') {
-            console.log( this)
-            this.setVelocity(0, 0)
-            return
-        }
-
-        let xVel = this.body.velocity.x;
-        let yVel = this.body.velocity.y;
-        if (this.direction == 0) {
-            yVel = -yVel;
-        } else if (this.direction == 1) {
-            this.facing = "left";
-
-            xVel = -xVel;
-        } else if (this.direction == 2) {
-            yVel = -yVel;
-        } else {
-            this.facing = "right";
-
-            xVel = -xVel;
-        }
-        // play the correct animation
-        if (xVel === 0 && yVel === 0) {
-            if (this.facing === "left") {
-                
-                this.anims.play("player-idle-left", true);
+        
+            if (this.direction === 'right') {
+                this.direction = 'left'
+                this.facing = 'left'
+            
             } else {
-                this.anims.play("player-idle-right", true);
-            }
-        } else {
-            if (this.facing === "left") {
-                
-                this.anims.play("player-walk-left", true);
-            } else {
-                this.anims.play("player-walk-right", true);
-            }
-        }
-        this.setVelocity(xVel, yVel);
-        // this.body.velocity.x = xVel
-        // this.body.velocity.y = yVel
-
-        return Math.Between(0, 3);
+                this.direction = 'right'
+                this.facing = 'right'
+           }
+    
+        
     }
 
     handleCollision() {
@@ -222,51 +190,20 @@ export class bossSprite extends Physics.Arcade.Sprite {
         const velocity = 20
         let xVel = this.body.velocity.x;
         let yVel = this.body.velocity.y;
-        this.scene.time.delayedCall(3500, () => {
-            
-            if (this.facing === 'right') {
-                this.facing = 'left'
-                console.log('this thing working? left', this.facing)
-                xVel = -velocity
-            } else {
-                this.facing = 'right'
-                
-                xVel = velocity
-                console.log('this thing working? right')
-            }
-            console.log(xVel)
-        })
-        if (this.direction === 0) {
-            //yVel = -velocity;
-        } else if (this.direction === 1) {
-            this.facing = "right";
-
-            //xVel = velocity;
-        } else if (this.direction === 2) {
-            //yVel = velocity;
-        } else {
-            this.facing = "left";
-
-            //xVel = -velocity;
-        }
-        // play the correct animation
-        if (xVel === 0 && yVel === 0) {
-            if (this.facing === "left") {
-                this.anims.play("player-idle-left", true);
-            } else {
-                this.anims.play("player-idle-right", true);
-            }
-        } else {
+      
+      
             if (this.facing === "left") {
                 this.anims.play("player-walk-left", true);
+                xVel = -velocity
             } else {
                 this.anims.play("player-walk-right", true);
+                xVel = velocity
             }
-        }
+        //}
         
         
         this.setVelocity(xVel, yVel);
-        console.log('velocity ', xVel, yVel)
+        
     }
     // soundEffectManager() {
     //     this.scene.sound.play('npcHurt')
